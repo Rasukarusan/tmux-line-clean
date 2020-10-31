5bfc589859e21f4f6851400834c9a35b332036e0
Rasukarusan <gta_youtube@yahoo.co.jp>
2020-11-01 01:22:23 +0900

    Remove visualMode and refactor

--- terminal-line-delete.sh
+++ terminal-line-delete.sh
@@ -9,22 +9,26 @@ quit_tty() {
 }
 
 getchar() {
-    dd bs=1 count=1 2> /dev/null
+  dd bs=1 count=1 2> /dev/null
 }
 
 up_cursor() {
-    tput cuu1
+  tput cuu1
 }
 
 down_cursor() {
-    tput cud1
+  tput cud1
 }
 
-clear_input() {
-    tput el
+clear_line() {
+  tput el
 }
 
-get_cursor_position() {
+undo_line() {
+  sed -n ${y}p $tmuxBuffer | tr -d '\n'
+}
+
+set_cursor_position() {
   old_settings=$(stty -g) || exit
   stty -icanon -echo min 0 time 3 || exit
   printf '\033[6n'
@@ -35,62 +39,51 @@ get_cursor_position() {
   stty "$old_settings"
 }
 
-get_cursor_position
-startLine=$y
-init_tty
-tmuxBuffer=$(mktemp tmux.XXXX)
-tmux capture-pane -pe > $tmuxBuffer
+restore() {
+  tput cup 0 0
+  sed -n 1,${restoreLine}p $tmuxBuffer
+  remove_tmux_buffer
+}
 
-isVisualMode=0
-while true ; do
-    char=$(getchar)
-    if [ $isVisualMode -eq 0 ]; then
-      get_cursor_position
-    fi
+ready_for_restore() {
+  trap "restore && exit" 2
+  set_cursor_position
+  restoreLine=$y
+}
 
+create_tmux_buffer() {
+  tmuxBuffer=$(mktemp)
+  tmux capture-pane -pe > $tmuxBuffer
+}
+
+remove_tmux_buffer() {
+  rm $tmuxBuffer
+}
+
+main() {
+  ready_for_restore
+  init_tty
+  create_tmux_buffer
+  while true ; do
+    set_cursor_position
+    local char=$(getchar)
     case "${char}" in
-        j)
-          down_cursor
-          line=$(expr $line + 1)
-          bufff=$(sed -n ${line}p $tmuxBuffer | tr -d "\n")
-          printf "\e[3m${bufff}\e[33m"
-          # tput setab 1 && printf "$bufff" 
-            ;;
-        k)
-          up_cursor
-          # clear_input
-            ;;
-        $'\x0d') # ENTER
-            get_cursor_position
-            tput cup 0 0
-            printf "\e[0m\e[0m"
-
-            sed -n 1,$(expr ${start} - 1)p $tmuxBuffer
-            tput cup $(expr $start - 1) 0
-            printf "\e[0m\e[0m"
-            deleteCnt=$(expr $startLine - $y)
-            for i in `seq 0 $deleteCnt`; do
-              tput cup $(expr $start + $i) 0
-              clear_input
-            done
-            tput cup $(expr $start - 1) 0
-            sed -n $(expr ${y} + 1),${startLine}p $tmuxBuffer
-            #
-            printf "\e[0m\e[0m"
-            # tail -n +$(expr ${y} + 1) $tmuxBuffer 
-            break
-            ;;
-        V) 
-          isVisualMode=1
-          start=$y
-          line=$y
-          bufff=$(sed -n ${y}p $tmuxBuffer | tr -d "\n")
-          printf "\e[3m${bufff}\e[3m"
-          ;;
-        *)
-          # echo "other"
+      j)
+        undo_line
+        down_cursor
+        ;;
+      k)
+        up_cursor
+        clear_line
+        ;;
+      $'\x0d') # ENTER
+        break
+        ;;
+      *)
+        ;;
     esac
-done
-
-rm $tmuxBuffer
-quit_tty
+  done
+  remove_tmux_buffer
+  quit_tty
+}
+main
